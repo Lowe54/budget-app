96cd5af4225492258b72ee6e08989439cd236b29
James Lowe <jameslowe241@gmail.com>
2020-06-22 14:10:05 +0100

    Budget Controller added

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+var budgetController = (function(){
+
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+   
+
+    var data = {
+       allItems: {
+           exp: [],
+           inc: []
+       },
+       totals: {
+           exp: 0,
+           inc: 0
+       }
+    };
+
+    return {
+        addItem: function() {
+            
+        }
+    }
+
+})();
+
 
 var UIController = (function(){
     var DOMstrings = {
@@ -41,7 +76,6 @@ var controller = (function(UICtrl){
 
     var ctrlAddItem = function() {
         var input = UICtrl.getInput();
-        console.log(input);
     };
 
     return {
