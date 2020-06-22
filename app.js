5d8a15e473bc518f3e5d65fe1bffd5230eb1cfe1
James Lowe <jameslowe241@gmail.com>
2020-06-22 12:13:18 +0100

    init function added, removed un-needed console logs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var UIController = (function(){
         inputDescription: '.add__description',
         inputValue: '.add__value',
         addButton: '.add__btn'
-    }
+    };
 
     return {
         getInput: function() {
@@ -13,29 +13,44 @@ var UIController = (function(){
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
-            }
+            };
         },
 
         getDOMStrings: function() {
-            return DOMstrings
+            return DOMstrings;
         }
-    }
+    };
 })();
 
 var controller = (function(UICtrl){
-    var DOMstrings = UICtrl.getDOMStrings();
+
+    function setupEventListeners() {
+        var DOMstrings = UICtrl.getDOMStrings();
+
+        document.querySelector(DOMstrings.addButton).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    }
+
+
+   
 
     var ctrlAddItem = function() {
         var input = UICtrl.getInput();
-        console.log(input)
-    }
+        console.log(input);
+    };
 
-    document.querySelector(DOMstrings.addButton).addEventListener('click', ctrlAddItem)
-    
-    document.addEventListener('keypress', function(){
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init: function(){
+            console.log('Application Started');
+            setupEventListeners();
         }
-    })
+    };
+    
 })(UIController);
 
+controller.init();
