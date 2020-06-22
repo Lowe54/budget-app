1c10584b99a4f84618617ff05a8f8e79f536ef01
James Lowe <jameslowe241@gmail.com>
2020-06-22 12:05:16 +0100

    Fixed typos with a few variables, added getDOMStrings function to UIController

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
 var UIController = (function(){
     var DOMstrings = {
         inputType: '.add__type',
-        inputDescription: '.add_description',
-        inputvalue: '.add__value'
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addButton: '.add__btn'
     }
 
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).nodeValue,
+                type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+
+        getDOMStrings: function() {
+            return DOMstrings
         }
     }
 })();
